fc823c2981e7ce8c39b5ad4d989e3a03b030e0e9
Andrii Lypak <say.elsword@gmail.com>
2024-03-09 17:49:03 +0200

    first_commit

---
+++ notes/noteapp/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+app_name = 'noteapp'
+
+urlpatterns = [
+    path('', views.main, name='main'),
+    path('note/', views.note, name='note'),
+    path('tag/', views.tag, name='tag'),
+    path('detail/<int:note_id>', views.detail, name='detail'),
+]
